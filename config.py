9de476d811e1fb3cb557903e87cf565c8d0a809a
Arseni Borisovs <aborisov@sfu.ca>
2020-01-17 18:58:54 -0800

    Add secret for wtforms

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    WTF_CSRF_SECRET_KEY = os.environ.get('WTF_SECRET_KEY')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
